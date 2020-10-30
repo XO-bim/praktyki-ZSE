874cfbf5ea0f9335fdd572c5b7d42a5e72cb3205
XO-bim <fallenwhisperbiznes@gmail.com>
2020-10-30 13:52:58 +0100

    FajnePraktyki

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+    int a = 2 , b = 3;
+
+   System.out.println("Wartość a jest = " + a);
+
+   System.out.println("Wartość b jest = " + b);
+
+   System.out.println("Suma wynosi " + (a+b));
+
+    }
+}
